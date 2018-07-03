caee8f50cb6b7c81b998296bd32d8f1402bb4891
jordan3900 <m.iordanize@gmail.com>
2018-07-03 11:40:23 +0300

    Git commit

--- EXERCISE FETCHING RESULTSETS WITH ADO.NET/01.InitialSetUp/Configuration.cs
+++ EXERCISE FETCHING RESULTSETS WITH ADO.NET/01.InitialSetUp/Configuration.cs
@@ -7,6 +7,6 @@ namespace _01.InitialSetUp
 {
     public static class Configuration
     {
-        public const string ConnectionString = @"Server=DESKTOP-5C8NCTB\SQLEXPRESS;Database=MinionsDB;Integrated Security=true";
+        public const string ConnectionString = @"Server=.\SQLEXPRESS;Database=MinionsDB;Integrated Security=true";
     }
 }
