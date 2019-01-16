146d6c8c24a41a35147e146b59871896586ab884
Yordan <pm.yordan@gmail.com>
2019-01-16 13:50:55 +0200

    Git commit.

--- AdvanceQuerying/BookShop.Data/Configuration.cs
+++ AdvanceQuerying/BookShop.Data/Configuration.cs
@@ -1,7 +1,12 @@
-﻿namespace BookShop.Data
-{
-    internal class Configuration
-    {
-        internal static string ConnectionString => @"Server=.;Database=BookShop;Integrated Security=True;";
-    }
-}
+﻿Using our sports data feeds and sports API, a complete solution delivering betting odds, scores and real-time settlement, we help multiple businesses meet their targets, from bookmakers to affiliates and from odds comparison sites to score sites. We can offer you different packages so you can fully customize your odds selection with your favorite sports, markets, and events.
+
+Focus on monitoring and updating different sports events.
+Trained and introduced the different sports and work process to more than 50 new co-workes
+Writing reports about the problems of the match or the statistician performance after every sports event covert from me
+Dealing with statistician problems over the phone.
+
+Allianz is a global leader in travel insurance and assistance services, life insurance, healthcare and other industries.
+
+Besides being a global leader in travel insurance and assistance services, Allianz Partners also supports numerous global lines of business, which are involved with automotive, healthcare, life insurance, and other industries.
+
+ne_e_truden_chestno
